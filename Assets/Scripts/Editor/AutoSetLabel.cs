9fee97fa705ac4a6990959a452917f8d1e78d5b2
yangfu <>
2022-07-08 14:20:01 +0800

    no message

--- Assets/Scripts/Editor/AutoSetLabel.cs
+++ Assets/Scripts/Editor/AutoSetLabel.cs
@@ -12,24 +12,12 @@ public class AutoSetLabel
     {  
         string rootPath = Application.dataPath + "/ABRes/";
         DirectoryInfo dirTempInfo = new DirectoryInfo(rootPath);
-        RecursionDir(dirTempInfo,"ABRes");
-        // FileSystemInfo [] dirSceneDirInfo = dirTempInfo.GetFileSystemInfos();
-
-        // string[] dirs = Directory.GetDirectories(rootPath);
-        // AssetDatabase.RemoveUnusedAssetBundleNames();
-        // foreach (FileSystemInfo dirInof in dirSceneDirInfo)
-        // {
-        //     FileInfo file = dirInof as FileInfo;
-        //     if (file != null)
-        //     {
-        //         Debug.Log("file name:" + file.Name);
-        //         // file.Name    
-        //     }
-        //     else
-        //     {
-        //         Debug.Log("dir name:" + dirInof.Name);
-        //     }
-        // }
+        DirectoryInfo[] dirSceneDirInfo = dirTempInfo.GetDirectories();
+        AssetDatabase.RemoveUnusedAssetBundleNames();
+        foreach (DirectoryInfo dirInof in dirSceneDirInfo)
+        {
+            RecursionDir(dirInof, dirInof.Name);
+        }
 
         AssetDatabase.Refresh();
     }
@@ -46,16 +34,57 @@ public class AutoSetLabel
             FileInfo file = _fileSystemInfo as FileInfo;
             if (file != null)
             {
-                Debug.Log("file name:"+file.Name);
+                if (file.Name.EndsWith(".meta"))
+                {
+                    continue;
+                }
+                AutoSetAssetBundleName(file, moudleName);
             }else
             {
-                Debug.Log("dir name:" + _fileSystemInfo.Name);
                 RecursionDir(_fileSystemInfo,moudleName);
             }
         }
     }
 
-    static void AutoSetAssetBundleName(){
+    static void AutoSetAssetBundleName(FileInfo file,string moudleName )
+    {
+        string strABName = string.Empty;
+        string strABPath = string.Empty;
+        string filePath = file.FullName;
+        string fileName = file.Name;
+
+        strABPath = filePath.Replace(Application.dataPath, "Assets");
+        strABName = moudleName + "/" + fileName;
+        int _index = strABPath.LastIndexOf(moudleName);
+        if (_index == -1)
+        {
+           return;
+        }
+        var tmpStr = strABPath.Substring(_index + moudleName.Length + 1);
+        tmpStr = tmpStr.Replace("\\", "/");     //替换\为/  windows下的路径
+        string [] strArr = tmpStr.Split('/');
+        if (strArr.Length > 0)
+        {
+            strABName = moudleName + "/" + strArr[0];
+        }
+        if (File.Exists(strABPath))
+        {
+            AssetImporter assetImporter = AssetImporter.GetAtPath(strABPath); //获取AssetImporter
+            assetImporter.assetBundleName = strABName;  //设置AssetBundle名称
+            assetImporter.assetBundleVariant = "ab";    //设置AssetBundle的Variant名称
+        }
+    }
+
+    [MenuItem("Tools/BuildAB")]
+    static void BuildAssetBundle()
+    {
+        string outPath = Application.streamingAssetsPath;
+        if ( ! Directory.Exists(outPath) )
+        {
+            Directory.CreateDirectory(outPath);
+        }
+
+        BuildPipeline.BuildAssetBundles(outPath,BuildAssetBundleOptions.None,BuildTarget.StandaloneOSX);
 
     }
 }
